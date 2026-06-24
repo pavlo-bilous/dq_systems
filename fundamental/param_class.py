9b23b25a9731da3120206f2c018453777cf75caa
Pavlo Bilous <pavbi@zpg003.bc.rzg.mpg.de>
2026-06-24 12:24:24 +0200

    Corrected imag and real props in param_class

--- fundamental/param_class.py
+++ fundamental/param_class.py
@@ -1,14 +1,16 @@
+from dataclasses import dataclass
 from collections.abc import Callable
 from numbers import Complex
 
 import dynamiqs as dq
 
 
+_bad_op_err_msg = "Bad operation: Time-dependend parameters can only be multiplied with dynamiqs objects."
+
+
+@dataclass(frozen=True)
 class Param(Complex):
-    
-    def __init__(self, p: complex | Callable[[float], complex]):
-        self.p = p
-        
+    p: complex | Callable[[float], complex]
         
     def __repr__(self):
         return f"Param({self.p})"
@@ -31,7 +33,7 @@ class Param(Complex):
             if isinstance(other, dq.QArrayLike):
                 return dq.modulated(self.p, other)
             else:
-                raise TypeError("Time-dependend parameter can only be multiplied with dynamiqs object.")
+                raise TypeError(_bad_op_err_msg)
         else:
             return self.p * other
         
@@ -42,7 +44,14 @@ class Param(Complex):
     
     
 def _delegate_op(self, op_name, *args, **kwargs):
-    return getattr(self.p, op_name)(*args, **kwargs)
+    if isinstance(self.p, Callable):
+        raise TypeError(_bad_op_err_msg)
+    op = getattr(self.p, op_name)
+    if op_name in "imag real".split():
+        res = op
+    else:
+        res = op(*args, **kwargs)
+    return res
 
 
 def _write_op_str(op_name):
@@ -50,6 +59,8 @@ def _write_op_str(op_name):
 f"""def {op_name}(self, *args, **kwargs):
     return _delegate_op(self, '{op_name}', *args, **kwargs)"""
     )
+    if op_name in "imag real".split():
+        op = "@property\n" + op
     return op
     
     
