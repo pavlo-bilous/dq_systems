9c4f19aec617f9495bf0f8c49db1222228717eaa
Pavlo Bilous <pavbi@zpg003.bc.rzg.mpg.de>
2026-06-24 12:38:23 +0200

    implementation of fundamental via Param improved

--- fundamental/resonator_mode.py
+++ fundamental/resonator_mode.py
@@ -9,15 +9,11 @@ from .param_class import *
 @dataclass(kw_only=True)
 class ResonatorMode(SimpleSystemABC):
     omega: float | Callable
-    omega_rwa: float | Callable
     
     
     def __post_init__(self):
         self.omega = Param(self.omega)
-        self.omega_rwa = Param(self.omega_rwa)
         
     
     def Hamiltonian(self):
-        H = self.omega * dq.number(self.N)
-        H_rwa = self.omega_rwa * dq.number(self.N)
-        return H - H_rwa
+        return self.omega * dq.number(self.N)
